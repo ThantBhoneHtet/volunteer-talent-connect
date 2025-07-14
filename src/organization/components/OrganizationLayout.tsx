6f455c4bac551f8d4bcb5770b795f66a7d67815b
Lesleyrayus <153513924+Lesleyrayus@users.noreply.github.com>
2025-07-15 01:53:00 +0630

    --

--- src/organization/components/OrganizationLayout.tsx
+++ src/organization/components/OrganizationLayout.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/shared/components/ui/button";
 import { Input } from "@/shared/components/ui/input";
 import { Outlet } from "react-router-dom";
 
-//just testing
+
 
 interface LayoutProps {
   children: React.ReactNode;
